b17c982825ab07a24e86d06018072e83719d8544
Pkant123689 <pushkarkant774@gmail.com>
2025-02-01 14:52:34 -0800

    sql_credit_card_analysis.sql

--- sql_credit_card_analysis.sql
+++ sql_credit_card_analysis.sql
@@ -15,8 +15,11 @@ from
 cte 
 cross join total_expenditure
 order by total_spent desc
+  
 
 --2- write a query to print highest spend month and amount spent in that month for each card type
+
+  
 select * from credit_card_transactions
 with cte as (
 select card_type, month(transaction_date) as transaction_month, year(transaction_date) as transaction_year, sum(amount) as total_amount
@@ -33,13 +36,18 @@ where rn = 1
 
 
 --3- write a query to find percentage contribution of spends by females for each expense type
+
+  
 select exp_type,
 sum(case when gender='F' then amount else 0 end)*100.0/sum(amount) as percentage_female_contribution
 from credit_card_transactions
 group by exp_type
 order by percentage_female_contribution desc;
 
+
 --4- during weekends which city has highest total spend to total no of transcations ratio 
+
+
 select top 1 city , sum(amount)*1.0/count(1) as ratio
 from credit_card_transactions
 where datepart(weekday,transaction_date) in (1,7)
